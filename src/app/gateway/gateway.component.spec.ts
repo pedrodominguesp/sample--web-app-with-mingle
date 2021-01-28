c7708fa46bb7f5b9bf1cca03a559d4878fee932b
Pedro Domingues <domingues.pedro@totvs.com.br>
2021-01-28 15:08:36 -0300

    x

--- src/app/gateway/gateway.component.spec.ts
+++ src/app/gateway/gateway.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GatewayComponent } from './gateway.component';
 
@@ -6,7 +6,7 @@ describe('GatewayComponent', () => {
   let component: GatewayComponent;
   let fixture: ComponentFixture<GatewayComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GatewayComponent ]
     })
