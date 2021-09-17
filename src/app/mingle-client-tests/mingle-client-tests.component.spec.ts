020c743e34e6e595a07c40338edadbc975799068
Pedro Domingues <domingues.pedro@totvs.com.br>
2021-09-17 15:21:31 -0300

    atualiza versao do framework Angular

--- src/app/mingle-client-tests/mingle-client-tests.component.spec.ts
+++ src/app/mingle-client-tests/mingle-client-tests.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MingleClientTestsComponent } from './mingle-client-tests.component';
 
@@ -6,7 +6,7 @@ describe('MingleClientTestsComponent', () => {
   let component: MingleClientTestsComponent;
   let fixture: ComponentFixture<MingleClientTestsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MingleClientTestsComponent ]
     })
